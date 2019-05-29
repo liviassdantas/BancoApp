5afddce3488b4d74f353d61f20980bf44142c698
Livia Dantas <livia.ss.dantas@gmail.com>
2019-05-29 13:06:07 -0300

    Tela de login pronta

---
+++ app/src/main/java/com/example/bancoapp/NavigationDrawer.java
@@ -0,0 +1,80 @@
+package com.example.bancoapp;
+
+import android.os.Bundle;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class NavigationDrawer extends AppCompatActivity implements
+        NavigationView.OnNavigationItemSelectedListener{
+    private Toolbar toolbar;
+    private DrawerLayout drwlayout;
+    private NavigationView nvgtview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navdrawer_activity_main);
+
+        //inicializando a toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //inicializando o drawer layout
+        drwlayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        //toggle
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle
+                (this, drwlayout, toolbar, R.string.abrir_drawer, R.string.fechar_drawer);
+        drwlayout.addDrawerListener(toogle);
+
+        toogle.syncState();
+
+        //click do navigation
+        nvgtview = (NavigationView) findViewById(R.id.navigationView);
+        nvgtview.setNavigationItemSelectedListener(this);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.ContainerFragment, new TelaLogin(), "TelaLogin")
+                .commit();
+    }
+
+    //botão voltar
+    @Override
+    public void onBackPressed() {
+        if (drwlayout.isDrawerOpen(GravityCompat.START)) {
+            drwlayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            //caso item 1
+            case R.id.item1: {
+                Toast.makeText(this, "Menu 1", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            //caso item 2
+            case R.id.item2: {
+                Toast.makeText(this, "Menu 2", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            default: {
+                Toast.makeText(this, "Menu", Toast.LENGTH_SHORT).show();
+            }
+        }
+        //fechando a navigation bar
+        drwlayout.closeDrawer(GravityCompat.START);
+        return true;
+
+    }
+}
---
+++ app/src/main/java/com/example/bancoapp/TelaLogin.java
@@ -0,0 +1,32 @@
+package com.example.bancoapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class TelaLogin extends Fragment {
+    private TextView txtBemvindo;
+    private EditText login;
+    private EditText senha;
+    private Button   btnlogar;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_tela_login, container, false);
+        txtBemvindo = view.findViewById(R.id.txtbemvindo);
+        login = view.findViewById(R.id.fragLogin);
+        senha = view.findViewById(R.id.fragSenha);
+        btnlogar = view.findViewById(R.id.btnlogar);
+
+        return view;
+    }
+
+}
